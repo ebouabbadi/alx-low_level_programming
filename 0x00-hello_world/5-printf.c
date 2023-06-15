c59dbad09f55c7f1f88488c6c3a23c04d3c25186
el mehdi bouabbadi <ebouabba1337@gmail.com>
2023-06-15 20:54:05 +0100

    my first push

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/*
+/**
  * main - A program that prints a string using printf.
  *
  * Return: Always 0 (Success)
-*/
-int main(void)
+ */
+int	main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
