ffefef5fe0fa0686c15455a6146e286375546a39
el mehdi bouabbadi <ebouabba1337@gmail.com>
2023-06-15 20:32:49 +0100

    my first push

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+Hello, puts
+*/
+
+int main(void)
+{
+    puts("Programming is like building a multilingual puzzle\n");
+    return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+Hello, printf
+*/
+
+int main(void)
+{
+    printf("Programming is like building a multilingual puzzle\n");
+    return (0);
+}
