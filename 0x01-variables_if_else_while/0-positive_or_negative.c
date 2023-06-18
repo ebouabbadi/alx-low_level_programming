0713422545e98c2eed51fe903135cb30118fa307
EL MEHDI BOUABBADI <ebouabba1337@gmail.com>
2023-06-18 10:29:59 -0700

    first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,9 @@
 #include <stdio.h>
 
 /**
- * main A  program will assign a random number to the variable n
+ * main - A  program
+ *
+ * Description: will assign a random number to the variable n
  * each time it is executed.
  * Complete the source code in order to print whether the number stored
  * in the variable n is positive or negative.
