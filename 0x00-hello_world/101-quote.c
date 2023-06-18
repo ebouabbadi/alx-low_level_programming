ba0f157c4325cadab0f1e062b588ada9c64fb673
EL MEHDI BOUABBADI <ebouabba1337@gmail.com>
2023-06-18 10:09:49 -0700

    first commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <string.h>
 
 /**
- * program main that prints
+ * main - A program that prints a string use function write.
  *
- * Return Always (1)
+ * Return: Always 1 (Failure)
  */
 int main(void)
 {
-	write( 1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"));
 	return (1);
 }
