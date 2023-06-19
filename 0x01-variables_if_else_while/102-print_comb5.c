928d14e4563a2295bf4c5ab7b3bacd3c517aef17
EL MEHDI BOUABBADI <ebouabba1337@gmail.com>
2023-06-19 16:13:07 -0700

    first commit

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for a program that print all possible
+ * different combinations of two two-digit numbers.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i + 1; j < 100; j++)
+		{
+			putchar(i / 10 + 48);
+			putchar(i % 10 + 48);
+			putchar(' ');
+			putchar(j / 10 + 48);
+			putchar(j % 10 + 48);
+
+			if (i != 98)
+			{
+				putchar(',');
+				putchar(' ');
+
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
+
