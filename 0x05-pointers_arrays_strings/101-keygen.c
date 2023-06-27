8442c6c12a5ab79d75968bb4bde23f09f1e56ee3
EL MEHDI BOUABBADI <ebouabba1337@gmail.com>
2023-06-26 18:42:16 -0700

    first commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,4 +13,12 @@ int main(void)
 	srand((unsigned int) time(&t));
 	while (c < 2772)
 	{
-
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
+}
