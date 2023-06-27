0ad95aab6c2ea52a9fe3500c935914e0488935c8
EL MEHDI BOUABBADI <ebouabba1337@gmail.com>
2023-06-26 18:36:22 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - generates keygen.
+ * Return: 0 Always.
+ */
+int main(void)
+{
+	int r = 0, c = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+
